4a8f4ed29822d619b79a56e4fee642d88bb1c640
DaniGomAris <danielgomezaristizabal2020@gmail.com>
2025-09-13 23:44:09 -0500

    change port

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const app = require("./src/app");
 const logger = require("./src/logger/logger");
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
   logger.info(`Running on http://localhost:${PORT}`);
